f83606da444003e9f78a6e390f70f3132509cbfd
Iryna Oliinyk <arairishka2@gmail.com>
2024-07-27 21:09:57 +0200

    svg

--- src/JS/about-me.js
+++ src/JS/about-me.js
@@ -1,7 +1,13 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+
+
 new Accordion('.accordion-container', {
   duration: 400,
   showMultiple: true,
 });
+
+
+
+
